ce50c31d03d53e70aaa8e16b2a89ddbb2bb30927
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-01-19 20:30:22 +0300

    monefaro
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- theme.ts
+++ theme.ts
@@ -1,38 +1,40 @@
 namespace $ {
-    /**
-     * Theme css variables
-     * @see https://mol.hyoo.ru/#!section=demos/demo=mol_textarea_demo
-     */
-    export const $bog_theme = $mol_style_prop('mol_theme', [
-        'back',
-        'hover',
-        'card',
-        'current',
-        'special',
-        'text',
-        'control',
-        'shade',
-        'line',
-        'focus',
-        'field',
-        'image',
-        'spirit',
-    ] as const)
+	/**
+	 * Theme css variables
+	 * @see https://mol.hyoo.ru/#!section=demos/demo=mol_textarea_demo
+	 */
+	export const $bog_theme = $mol_style_prop('mol_theme', [
+		'back',
+		'hover',
+		'card',
+		'current',
+		'special',
+		'text',
+		'control',
+		'shade',
+		'line',
+		'focus',
+		'field',
+		'image',
+		'spirit',
+	] as const)
 
-    /**
-     * Available theme names.
-     * Add new theme to theme.css and add its name here.
-     */
-    export const $bog_theme_names = [
-        '$mol_theme_light',
-        '$mol_theme_dark',
-        '$mol_theme_upwork',
-        '$mol_theme_ainews_light',
-        '$mol_theme_ainews_dark',
-    ] as const
+	/**
+	 * Available theme names.
+	 * Add new theme to theme.css and add its name here.
+	 */
+	export const $bog_theme_names = [
+		'$mol_theme_light',
+		'$mol_theme_dark',
+		'$mol_theme_monefro_light',
+		'$mol_theme_monefro_dark',
+		'$mol_theme_upwork',
+		'$mol_theme_ainews_light',
+		'$mol_theme_ainews_dark',
+	] as const
 
-    /**
-     * Type-safe theme name
-     */
-    export type $bog_theme_name = (typeof $bog_theme_names)[number]
+	/**
+	 * Type-safe theme name
+	 */
+	export type $bog_theme_name = (typeof $bog_theme_names)[number]
 }
